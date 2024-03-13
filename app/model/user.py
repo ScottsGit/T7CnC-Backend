cfe4203d5ef0c62bd1fb550a1847a6a2a2d4d5db
Yuan <whataever0317@gmail.com>
2024-03-13 13:54:19 -0400

    add user model

---
+++ app/model/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    email = Column(String, unique=True)
+    password = Column(String)
+    jwt_token = Column(String)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
