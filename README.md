5ad247bc9e2aa54a7854d45018ae7ddf5fe0742c
Yuan <whataever0317@gmail.com>
2024-03-13 13:10:20 -0400

    init backend with helloworld

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# T7CnC-Backend
-Team 7 - Cash-n-Crunch Backend
+Install Python  
+
+### Poetry  
+Poetry is like 'npm' for packages management for Python projects.  
+Install Poetry: https://python-poetry.org/docs/  
+
+### PostgreSQL
+Install PostgreSQL if you have not yet: https://www.postgresql.org/download/  
+
+### PGAdmin  
+PGAdmin is for PostgreSQL management.  
+
+### Setup Backend
+poetry install  
+poetry run start  
+Go to http://localhost:8888/docs  
---
+++ app/main.py
@@ -0,0 +1,32 @@
+import uvicorn
+from fastapi import FastAPI, APIRouter
+from app.config import db
+
+def init_app():
+    db.init()
+    
+    app = FastAPI(
+        title= "T7CnC-Backend",
+        description= "Login Page",
+        version= "1"
+    )
+
+    @app.on_event("startup")
+    async def starup():
+        await db.create_all()
+    
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.close()
+        
+    @app.get("/")
+    async def home():
+        return "hello world"
+        
+    return app
+
+app = init_app()
+
+def start():
+    """Launched with 'poetry run start' at root level """
+    uvicorn.run("app.main:app", host="localhost", port=8888, reload=True)
