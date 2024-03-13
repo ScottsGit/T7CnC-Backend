5a3f5044d495be825377c38d87f273a5240c96f5
Yuan <whataever0317@gmail.com>
2024-03-13 13:55:21 -0400

    add plaid_access_token to user model

--- app/model/user.py
+++ app/model/user.py
@@ -13,5 +13,6 @@ class User(Base):
     email = Column(String, unique=True)
     password = Column(String)
     jwt_token = Column(String)
+    plaid_access_token = Column(String)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
