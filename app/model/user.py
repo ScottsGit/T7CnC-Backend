905eb9becae8617dcc23dc046322a48c8938b1f9
Yuan <whataever0317@gmail.com>
2024-03-14 10:38:22 -0400

    fixes

--- app/model/user.py
+++ app/model/user.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, DateTime, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
+from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
-class Users(Base):
+
+class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True)
@@ -15,14 +16,3 @@ class Users(Base):
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
     
-    
-    
-from pydantic import BaseModel
-from datetime import datetime
-    
-class UserSchema(BaseModel):
-    username: str
-    jwt_token: str
-
-    class Config:
-        orm_mode = True
