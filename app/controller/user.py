a4a2f75cfae82dc59cb53b074dcbc24d51c627eb
Yuan <whataever0317@gmail.com>
2024-03-14 19:14:28 -0400

    change user_id to user_email for get /users

--- app/controller/user.py
+++ app/controller/user.py
@@ -15,12 +15,12 @@ router = APIRouter(
 )
 
 
-@router.get("/{user_id}", response_model=ResponseSchema)
+@router.get("/{user_email}", response_model=ResponseSchema)
 async def read_user_by_id(
-    user_id: str, current_user: UserInDB = Depends(AuthService.get_current_user)):
+    user_email: str, current_user: UserInDB = Depends(AuthService.get_current_user)):
     
-    user = await UserService.get_user_profile_by_id(user_id)
+    user = await UserService.find_by_email(user_email)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     
-    return ResponseSchema(detail="Successfully!", result=UserBase(**user))
+    return ResponseSchema(detail="Successfully!", result=user)
